59ffe90317231c3ab49b2fcd296cfde28d1b2e2b
Guilherme <guilherme1301703@fapi-pinhais.edu.br>
2018-03-07 17:48:14 -0300

    first commit for Series DATABASE, it countains the sql script to create the database, and a spreedsheet that has the data structure info

---
+++ db/SERIESDE.sql
@@ -0,0 +1,122 @@
+CREATE TABLE genres
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(100),
+	description TEXT,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE contents
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(100),
+	description TEXT,
+	PRIMARY KEY(id)
+)
+
+CREATE TABLE franchises
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(100),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE serietypes
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	type VARCHAR(100),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE series
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(100),
+	score INT,
+	released DATE,
+	description TEXT,
+	franchise INT,
+	type INT,
+	PRIMARY KEY(id),
+	FOREIGN KEY(franchise) REFERENCES franchises(id),
+	FOREIGN KEY(type) REFERENCES serietypes(id)
+);
+
+CREATE TABLE genresseries
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	gid int,
+	sid int,
+	PRIMARY KEY(id),
+	FOREIGN KEY(gid) REFERENCES genres(id),
+	FOREIGN KEY(sid) REFERENCES series(id)
+);
+
+CREATE TABLE contentsseries
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	cid int,
+	sid int,
+	PRIMARY KEY(id),
+	FOREIGN KEY(cid) REFERENCES contents(id),
+	FOREIGN KEY(sid) REFERENCES series(id)
+);
+
+CREATE TABLE traits
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(100),
+	description TEXT,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE appearances
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(100),
+	description TEXT,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE characters
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(100),
+	img LONGBLOB,
+	description TEXT,
+	age INT,
+	gender INT,
+	main INT,
+	PRIMARY KEY(id),
+	FOREIGN KEY(font) REFERENCES series(id)
+);
+
+CREATE TABLE characterstraits
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	tid int,
+	cid int,
+	PRIMARY KEY(id),
+	FOREIGN KEY(tid) REFERENCES traits(id),
+	FOREIGN KEY(cid) REFERENCES characters(id)
+);
+
+CREATE TABLE charactersseries
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	cid int,
+	sid int,
+	PRIMARY KEY(id),
+	FOREIGN KEY(cid) REFERENCES characters(id),
+	FOREIGN KEY(sid) REFERENCES series(id)
+);
+
+CREATE TABLE charactersappearances
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	aid int,
+	cid int,
+	PRIMARY KEY(id),
+	FOREIGN KEY(aid) REFERENCES appearances(id),
+	FOREIGN KEY(cid) REFERENCES characters(id)
+);
